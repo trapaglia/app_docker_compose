cf248a4c9dae5a5e3962b22504428a10a3b4a4fb
Pichi Loco <matiasdmansilla@hotmail.com>
2020-11-22 20:10:38 -0300

    locust stress ready

--- stress_test/locustfile.py
+++ stress_test/locustfile.py
@@ -5,11 +5,12 @@ class QuickstartUser(HttpUser):
 
     @task(1)
     def index(self):
-        raise NotInplementedError
+        self.client.get('/')
 
     @task(3)
     def predict(self):
-        raise NotInplementedError
+        for i in range(4):
+            self.client.post('/predict', params={'text': 'dolar %d'%i})
 
     def on_start(self):
         pass
